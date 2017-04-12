d5854104da108a64d07d211a13ecd4d4653eb5fc
alumno <alumno@macbook-de-admin-2.local>
2017-04-11 20:53:19 -0500

    first commit

--- CameraGalaeryEnIOs/ViewController.swift
+++ CameraGalaeryEnIOs/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageViewPhoto: UIImageView!
+    
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +27,67 @@ class ViewController: UIViewController {
     }
 
 
+    func usarGaleria() {
+        /*
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+        
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = UImageController.availableMediaType(for: .photoLibrary)
+            
+            self.present(imagePicker, animated: true, completion: {})
+            
+        }
+        */
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var  chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        imageViewPhoto.contentMode = .scaleAspectFit //3
+        imageViewPhoto.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    
+    @IBAction func openCameraTouchUp(_ sender: AnyObject) {
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            
+            
+            //picker.modalPresentationStyle = .fullScreen
+            
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            imagePicker.cameraCaptureMode = .photo
+            present(imagePicker, animated: true,completion: nil)
+            
+            
+            
+            //self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    @IBAction func galeryTouchUp(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        //imagePicker.modalPresentationStyle = .popover
+        present(imagePicker, animated: true, completion: nil)
+        //imagePicker.popoverPresentationController?.barButtonItem = sender
+        }
+    }
+    
 }
 
